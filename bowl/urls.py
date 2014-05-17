e7b79c9d8536f8079ed79e6f87b5f5222b365e87
James T. Allen <j.allen@physics.usyd.edu.au>
2014-05-17 16:19:12 +1000

    Can logout.

--- bowl/urls.py
+++ bowl/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^game/', include('game.urls', namespace='game')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'bowl.views.logout_view', name='logout'),
 )
