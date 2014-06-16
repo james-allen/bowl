da9098c464abb0d3449fd2c0846867505ded0aa3
James T. Allen <j.allen@physics.usyd.edu.au>
2014-06-16 20:18:29 +1000

    Registered models with admin.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from game.models import Match, Team, Player, PlayerInGame, Challenge, Race
+from game.models import Position, Step
 
-# Register your models here.
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(PlayerInGame)
+admin.site.register(Challenge)
+admin.site.register(Race)
+admin.site.register(Position)
+admin.site.register(Step)
